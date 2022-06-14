b436325dcff87dbfd45c994154db08def09f20cf
Michael Lipscombe <mlipscombe@outlook.com>
2022-06-14 16:24:49 -0600

    Create test app

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/")]
+fn default() -> String {
+    format!("Hello World")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![default,hello])
+
+}
